c8750db4c375fdac486369e731cf72eda8f8373d
Lucas Viana <oviana@outlook.com>
2021-10-28 19:59:09 -0300

    Setup Gitlab CI's pipele

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 // eslint-disable-next-line
 import React from 'react'
 import './App.css'
-import logo from './logo.svg'
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
