cb76bd62cd4765ac99b9f0b9accd7f3500f144e6
Lucas Viana <oviana@outlook.com>
2021-11-01 13:49:46 -0300

    Add GitLab icon to footer

---
+++ src/components/core/footer/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './index.scss'
+
+export function Footer() {
+  return (
+    <footer className='mx-auto pb-2 w-100 px-4 px-md-5 text-center mw-1200px'>
+      <hr className='my-4 mx-auto'></hr>
+      <div className='px-4'>
+        <p>
+          © 2021 — Aplicação desenvolvida por Antônio Pedro Medrado (99999999),
+          Lucas Viana (10748409) e Yann Amado (99999999).
+        </p>
+        <a
+          href='https://gitlab.com/icmc-ssc0952/2021/giotgrad07'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='
+            text-no-decoration
+            text-muted d-flex
+            justify-content-center
+          '
+        >
+          <i className='icon fab fa-gitlab'></i>
+          Código-Fonte
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
