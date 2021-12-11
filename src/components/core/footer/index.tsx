dfe41f34ca0e8e6fa42e66380065f0a76fda4eb6
Lucas Viana <oviana@outlook.com>
2021-12-11 20:28:30 -0300

    Update footer info

--- src/components/core/footer/index.tsx
+++ src/components/core/footer/index.tsx
@@ -7,8 +7,8 @@ export function Footer() {
       <hr className='my-4 mx-auto'></hr>
       <div className='px-4'>
         <p>
-          © 2021 — Aplicação desenvolvida por Antônio Pedro Medrado (99999999),
-          Lucas Viana (10748409) e Yann Amado (99999999).
+          © 2021 — Aplicação desenvolvida por Antônio Pedro Medrado (10748389),
+          Lucas Viana (10748409) e Yann Amado (10746943).
         </p>
         <a
           href='https://gitlab.com/icmc-ssc0952/2021/giotgrad07'
